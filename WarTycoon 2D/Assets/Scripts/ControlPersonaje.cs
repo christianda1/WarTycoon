c98dcc68408488680a35218d9f2e73740cc19b47
christiada1 <c.david.granada@gmail.com>
2016-04-10 17:16:31 -0500

    Personaje de Prueba con animaciones

---
+++ WarTycoon 2D/Assets/Scripts/ControlPersonaje.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Personaje {
+	public class ControlPersonaje : MonoBehaviour {
+		
+		public float velocidadCaminar;
+		public float impulsoSalto;
+		
+		public Transform puntoVerificarPiso;
+		public LayerMask capaPiso;
+		
+		private Rigidbody2D bodyPersonaje;
+		private Vector2 movimiento;
+		
+		private bool saltarInput;
+		public float horizontalInput;
+		public bool estoyEnElPiso;
+
+		public CircleCollider2D cuerpoFisico;
+		
+		private Animator animacionPersonajePrueba;
+		private bool mirarLadoDerecho;
+		
+		
+		
+		// Use this for initialization
+		void Start () {
+
+
+			this.bodyPersonaje= this.GetComponent<Rigidbody2D>();
+			this.movimiento = new Vector2 ();
+			
+			this.estoyEnElPiso = false;
+			
+			this.animacionPersonajePrueba= this.GetComponent<Animator>();
+			this.mirarLadoDerecho = true;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+			
+			this.horizontalInput = Input.GetAxis ("Horizontal");
+			this.saltarInput = Input.GetKey(KeyCode.Space);
+			
+			this.animacionPersonajePrueba.SetFloat ("VelocidadHorizontal",Mathf.Abs(this.bodyPersonaje.velocity.x));
+			this.animacionPersonajePrueba.SetFloat ("VelocidadVertical", Mathf.Abs (this.bodyPersonaje.velocity.y));
+			
+			
+			if ((horizontalInput < 0.0f) && (this.mirarLadoDerecho)) {
+				this.Doblar ();
+				this.mirarLadoDerecho=false;
+			}else if((horizontalInput>0.0f) && (!this.mirarLadoDerecho)){
+				this.Doblar ();
+				this.mirarLadoDerecho=true;
+			}
+			
+			//Si estoy en el aire (No estoy tocando nada)
+			if (Physics2D.OverlapCircle (this.puntoVerificarPiso.position, 0.02f, this.capaPiso)) {
+				this.estoyEnElPiso = true;
+				//Debug.Log("ESTOY TOCANDO EL PSIO");
+
+				
+			} else {
+				this.estoyEnElPiso = false;
+
+				//Debug.Log("ESTOY EN EL AIRE");
+			}
+			
+			//FixedUpdate ();
+		}
+		
+		void FixedUpdate(){
+			this.movimiento = this.bodyPersonaje.velocity;
+			
+			this.movimiento.x = horizontalInput * velocidadCaminar;
+			
+			if(this.saltarInput==true && this.estoyEnElPiso==true){
+				this.movimiento.y=impulsoSalto;
+			}
+			//Para caer con Velocidad Constante = 8.0 y no acelerar en caidas Vmax=8.0
+			if(!estoyEnElPiso){
+				if(this.movimiento.y < -8.0f){
+					this.movimiento.y=-8.0f;
+				}
+			}
+			
+			this.bodyPersonaje.velocity = this.movimiento;
+			
+		}
+
+		void Doblar(){
+			Vector3 escala = this.transform.localScale;
+			escala.x *= (-1);
+			this.transform.localScale = escala;
+
+		}
+		
+	}
+}
